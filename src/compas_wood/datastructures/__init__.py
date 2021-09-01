0afc3dc47df9cb2b7ed260ee5b022db5ed9a2936
Petras Vestartas <petrasvestartas@gmail.com>
2021-09-01 14:11:52 +0200

    Documentation

---
+++ src/compas_wood/datastructures/__init__.py
@@ -0,0 +1,18 @@
+"""
+********************************************************************************
+compas_wood.datastructures
+********************************************************************************
+
+.. currentmodule:: compas_wood.datastructures
+
+Classes
+=======
+
+.. autosummary::
+    :toctree: generated
+    :nosignatures:
+
+
+"""
+
+from .assembly import Assembly
